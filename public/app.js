4807a0373a05e1701dad25fa1522fd9e9bf13ea2
swoboda <michal.swoboda@outlook.com>
2019-12-17 18:45:33 -0500

    added modal note editor

--- public/app.js
+++ public/app.js
@@ -70,3 +70,11 @@ $(document).on("click", "#savenote", function() {
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+
+$('#noteModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') 
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
